8ddd313cb2d11b35272040c3faf60593be5cfaa6
23subham <83632869+23subham@users.noreply.github.com>
2021-07-03 11:35:11 +0530

    Add files via upload

---
+++ Program41.java
@@ -0,0 +1,16 @@
+class Program41 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("java Program started");
+		boolean last= true;
+		int num1=27;
+		int num2=58;
+		if(num1%10==num2%10)
+		{
+		System.out.println("true");	
+		}
+		else
+		System.out.println("false");
+	}
+}
